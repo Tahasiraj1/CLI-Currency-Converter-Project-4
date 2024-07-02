feab26f25ba4fa622da6d2e9d2b76f071139c488
Tahasiraj1 <160783918+Tahasiraj1@users.noreply.github.com>
2024-07-02 11:26:48 +0500

    Add files via upload
    The TypeScript console app is used to convert currencies: the users enter a certain amount of money in one currency and set the currency they want to check the monetary value of.

---
+++ main.ts
@@ -0,0 +1,57 @@
+import inquirer from 'inquirer';
+
+let conversion = {
+    "PKR": {
+        "USD": 0.0036,
+        "CAD": 0.0049,
+        "PKR": 1
+    },
+    "USD": {
+        "CAD": 1.37,
+        "PKR": 278.31,
+        "USD": 1
+    },
+    "CAD": {
+        "USD": 0.73,
+        "PKR": 203.30, 
+        "CAD": 1
+    }
+};
+
+const answers: {
+    from: "PKR" | "USD" | "CAD",
+    to: "PKR" | "USD" | "CAD",
+    amount: number
+} = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'from',
+        choices: ["PKR", "USD", "CAD"],
+        message: 'Select your currency: '
+    },
+    {
+        type: 'list',
+        name: 'to',
+        choices: ["PKR", "USD", "CAD"],
+        message: 'Select your conversion currency: '
+    },
+    {
+        type: 'input',
+        name: 'amount',
+        message: 'Enter your amount: ',
+        validate: function(value) {
+            const valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number';
+        },
+        filter: Number
+    }
+]);
+
+const { from, to, amount } = answers;
+
+if (from && to && amount) {
+    let result = conversion[from][to] * amount;
+    console.log(`Your currency from ${from}, converted to ${to}, is ${result}`);
+} else {
+    console.log('Invalid input');
+}
